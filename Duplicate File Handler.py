b2e221d21e1b063c9b7994a17b43dfe6b30b6303
MichielSaey <33897625+MichielSaey@users.noreply.github.com>
2022-10-12 15:50:13 +0200

    Update Duplicate File Handler.py

--- Duplicate File Handler.py
+++ Duplicate File Handler.py
@@ -1,9 +1,7 @@
 # write your code here
 import argparse
 import os
-
-
-
+from typing import Dict
 
 
 def nonEmptyFile(path):
@@ -38,16 +36,95 @@ def read_input():
     return args
 
 
-def print_folder_structure(_path):
+def create_dictionary_folder_structure(_path):
+    dictionary: Dict[int, list] = dict()
     for root, dirs, files in os.walk(_path, topdown=False):
         for name in files:
-            print(os.path.join(root, name))
+            file = os.path.join(root, name)
+            size = os.stat(file).st_size
+
+            if size in dictionary:
+                file_list = dictionary[size]
+                file_list.append(file)
+                dictionary.update({size: file_list})
+            else:
+                dictionary.update({size: [file]})
+
+    return dictionary
+
+
+def read_file_type():
+    file_type = None
+    while file_type is None:
+        print("Enter file format:")
+        file_type = input()
+    return file_type
+
+
+def read_sort():
+    print("Size sorting options:")
+    print("1. Descending")
+    print("2. Ascending")
+
+    selection = 0
+    while True:
+        print("Enter a sorting option:")
+        selection = int(input())
+        if selection in [1, 2]:
+            return True if selection == 1 else False
+        else:
+            print("/nWrong Option:/n")
+
+
+def print_dictionary(dictionary):
+    for key, values in dictionary.items():
+        print(f'{key} bytes')
+        for file in values:
+            print(file)
+
+
+def filter_dictionary(dictionary, file_type):
+    new_dictionary: Dict[int, list] = dict()
+    for size, files in dictionary.items():
+
+        new_files = [file for file in files if file.endswith(file_type)]
+
+        if new_files:
+            new_dictionary.update({size: new_files})
+
+    return new_dictionary
+
+
+def sort_dictionary(dictionary, sort):
+    order = sorted(dictionary)
+    new_dictionary: Dict[int, list] = dict()
+
+    if sort:
+        order.reverse()
+
+    for i in order:
+        files = dictionary[i]
+        new_dictionary.update({i: files})
+
+    return new_dictionary
+
+
+def run():
+    args = read_input()
+    path = args.root_folder
+
+    if nonEmptyFile(path):
+        dictionary = create_dictionary_folder_structure(path)
+
+        file_type = read_file_type()
+        dictionary = filter_dictionary(dictionary, file_type)
+
+        sort = read_sort()
+        dictionary = sort_dictionary(dictionary, sort)
+        print_dictionary(dictionary)
 
 
 # Code
-args = read_input()
-path = args.root_folder
-
-if nonEmptyFile(path):
-    print_folder_structure(path)
+if __name__ == '__main__':
+    run()
 
