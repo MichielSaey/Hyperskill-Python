68aa16225bb9bb196388452efdc922eb4ad7cc34
MichielSaey <33897625+MichielSaey@users.noreply.github.com>
2022-10-17 12:42:46 +0200

    Update Duplicate File Handler.py

--- Duplicate File Handler.py
+++ Duplicate File Handler.py
@@ -1,10 +1,12 @@
 # write your code here
+# write your code here
 import argparse
 import os
 from typing import Dict
+from hashlib import md5
 
 
-def nonEmptyFile(path):
+def non_empty_file(path):
     if path is None or path == "":
         print("Directory is not specified")
         return False
@@ -62,7 +64,7 @@ def read_file_type():
 
 
 def read_sort():
-    print("Size sorting options:")
+    print("\nSize sorting options:")
     print("1. Descending")
     print("2. Ascending")
 
@@ -78,9 +80,23 @@ def read_sort():
 
 def print_dictionary(dictionary):
     for key, values in dictionary.items():
-        print(f'{key} bytes')
+        print(f'\n{key} bytes')
         for file in values:
             print(file)
+
+
+def print_hash_dictionary(dictionary):
+    idx = 1
+    for key, values in dictionary.items():
+        print(f'{key} bytes')
+
+        for hash_key, files in values.items():
+            print(f'Hash: {hash_key}')
+
+            for file in files:
+                print(f'{idx}. {file}')
+                idx += 1
+        print("")
 
 
 def filter_dictionary(dictionary, file_type):
@@ -109,11 +125,44 @@ def sort_dictionary(dictionary, sort):
     return new_dictionary
 
 
+def duplicates_finder(dictionary):
+
+    duplicates_dictionary: Dict[int, Dict[str, list]] = dict()
+
+    for key, values in dictionary.items():
+        found_hashes_with_files: Dict[str, list] = dict()
+        for value in values:
+            hex_m = ""
+
+            with open(value, 'rb') as f1:
+                m = md5()
+                m.update(f1.read())
+                hex_m = m.hexdigest()
+
+            if hex_m in found_hashes_with_files.keys():
+                updated_list = found_hashes_with_files.get(hex_m)
+                updated_list.append(value)
+                found_hashes_with_files.update({hex_m: updated_list})
+            else:
+                found_hashes_with_files.update({hex_m: [value]})
+
+        duplicates_hashes_with_files = {
+            _key: _value for (_key, _value) in found_hashes_with_files.items()
+            if len(_value) > 1
+        }
+
+        duplicates_dictionary.update({
+            key: duplicates_hashes_with_files
+        })
+
+    print_hash_dictionary(duplicates_dictionary)
+
+
 def run():
     args = read_input()
     path = args.root_folder
 
-    if nonEmptyFile(path):
+    if non_empty_file(path):
         dictionary = create_dictionary_folder_structure(path)
 
         file_type = read_file_type()
@@ -123,8 +172,13 @@ def run():
         dictionary = sort_dictionary(dictionary, sort)
         print_dictionary(dictionary)
 
+        print('\nCheck for duplicates?')
+        answer = input().strip()
+        if answer == 'yes':
+            print('')
+            duplicates_finder(dictionary)
+
 
 # Code
 if __name__ == '__main__':
     run()
-
